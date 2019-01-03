b5d316f481afbace1e1bceb2c36084eb168352e8
Christopher Jativa <cjativa@barnesfoundation.org>
2019-01-03 14:46:42 -0500

    added animation for add image request

--- src/components/addImageContainer/addImageView/addImageView.tsx
+++ src/components/addImageContainer/addImageView/addImageView.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react'
 import { PhotoView } from '../../photoView/photoView'
+import { LoadingDialog } from '../../../shared/components/loadingDialog'
 import { CreateResponse } from '../../../services/imageService'
 import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
 
 interface Props {
     photoUri: string
-    itemId: string
+    id: string
     addImageToItem: Function
 
-    addImageSuccess: boolean
-    addImageResponse: CreateResponse
+    success: boolean
+    response: CreateResponse
 
-    addImageRequestError: boolean
-    addImageRequestErrorMessage: any
+    error: boolean
+    errorMessage: any
 
-    addImageRequestComplete: boolean
+    requestComplete: boolean
+    requestInProgress: boolean
 }
 
 class AddImageView extends React.Component<Props, object> {
@@ -25,29 +28,34 @@ class AddImageView extends React.Component<Props, object> {
 
     public render() {
 
-        const photoView = (<PhotoView photoUri={this.props.photoUri}></PhotoView>)
+        const { photoUri, requestComplete, success, error, errorMessage, requestInProgress, id } = this.props
 
+        const displayText = 'Adding image to Catchoom'
+        const dialogOpen = requestInProgress
+
+        const photoView = (<PhotoView photoUri={photoUri}></PhotoView>)
         const addImageButton = (<div className="button-container"><Button variant="contained" onClick={this.addImageToItem}>Add Image to Item</Button></div>)
-
-        const successSection = (<p>Successfully added the image to item {this.props.itemId}</p>)
-
-        const failSection = (<p>Failed to add the image to item {this.props.itemId}</p>)
+        const successSection = (<p>Successfully added the image to item {id}</p>)
+        const failSection = (<p>Failed to add the image to item {id}</p>)
+        const cameraButton = (<Button variant="contained" component={({ innerRef, ...props }) => <Link {...props} to="/camera-capture" />}>Return to Camera</Button>)
 
         const errorSection = (
             <div>
                 <p>The error that occurred:</p>
-                <p>{this.props.addImageRequestErrorMessage}</p>
+                <p>{errorMessage}</p>
             </div>
         )
 
         return (
-            <>
+            <div className="add-image-container">
                 {photoView}
-                {(this.props.addImageRequestComplete) ? null : addImageButton}
-                {(this.props.addImageRequestComplete && this.props.addImageSuccess) ? successSection : null}
-                {(this.props.addImageRequestComplete && !this.props.addImageSuccess) ? failSection : null}
-                {(this.props.addImageRequestError) ? errorSection : null}
-            </>
+                {(requestComplete) ? null : addImageButton}
+                {(requestComplete && success) ? successSection : null}
+                {(requestComplete && !success) ? failSection : null}
+                {(error) ? errorSection : null}
+                {(requestComplete) ? cameraButton : null}
+                {(requestInProgress) ? <LoadingDialog displayText={displayText} dialogOpen={true}/> : null}
+            </div>
         )
     }
 
