c0eb2f3b85eb06a78bee1c7ca7e92d87a7389050
Christopher Jativa <cjativa@barnesfoundation.org>
2019-01-03 17:03:34 -0500

    modified cropper options and separated view elements into itemSearchView

--- src/components/itemSearchContainer/itemSearchView/itemSearchView.tsx
+++ src/components/itemSearchContainer/itemSearchView/itemSearchView.tsx
@@ -1,46 +1,57 @@
 import * as React from 'react'
 import { SearchResponse } from '../../../services/itemSearchService'
+import { ItemSearchForm } from '../itemSearchView/itemSearchForm'
+import Button from '@material-ui/core/Button'
 
 interface Props {
-    itemSearchResponse: SearchResponse,
-    itemSearchSuccess: boolean,
-    itemSearchRequestComplete: boolean,
-    searchedItemId: string,
-    setSearchedItem: Function
+    response: SearchResponse,
+    success: boolean,
+    requestComplete: boolean,
+    searchedId: string,
+    setSearchedItem: any
+    handleSubmit: Function
 }
 
 class ItemSearchView extends React.Component<Props> {
 
     constructor(props) { super(props) }
 
-    /** Sets the searched item into the state of the application */
-    setSearchedItem = (event) => { this.props.setSearchedItem() }
-
     public render() {
 
-        const matchResultText = ((this.props.itemSearchSuccess) ? 'Match found' : 'No match found')
+        const { requestComplete, success, searchedId } = this.props
+
+        const itemSearchForm = (<ItemSearchForm handleSubmit={this.props.handleSubmit}></ItemSearchForm>)
+        const setItemButton = (<Button variant="contained" onClick={this.props.setSearchedItem}>Set item</Button>)
+
+        const matchResultText = ((this.props.success) ? 'Match found' : 'No match found')
 
         const searchResultGrid = (
             <table>
                 <tbody>
                     <tr>
                         <th>ID:</th>
-                        <td>{this.props.itemSearchResponse.name}</td>
+                        <td>{this.props.response.name}</td>
                     </tr>
                     <tr>
                         <th>UUID:</th>
-                        <td>{this.props.itemSearchResponse.uuid}</td>
+                        <td>{this.props.response.uuid}</td>
                     </tr>
                 </tbody>
             </table>
         )
 
         return (
-            <div className="search-result">
-            <h2>Search By Item ID</h2>
-                <div className={(this.props.itemSearchRequestComplete) ? "unhidden" : "hidden"}>
-                    <h3>{(this.props.itemSearchRequestComplete) ? matchResultText : null}</h3>
-                    {searchResultGrid}
+            <div className="item-search-container">
+                <div className="search-result">
+                    <h2>Search By Item ID</h2>
+                    <div className={(this.props.requestComplete) ? "unhidden" : "hidden"}>
+                        <h3>{(this.props.requestComplete) ? matchResultText : null}</h3>
+                        {searchResultGrid}
+                    </div>
+                </div>
+                {itemSearchForm}
+                <div className="button-container">
+                    {(requestComplete && success) ? setItemButton : null}
                 </div>
             </div>
         )
