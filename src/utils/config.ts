0f58e7312ab7150eef0c909d94cb08de04bf76a7
Christopher Jativa <cjativa@barnesfoundation.org>
2022-02-02 00:25:12 -0500

    move constants from environment variables to here

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,21 +1,29 @@
 
+const REACT_APP_COLLECTION_EP='collection';
+const REACT_APP_ITEM_EP='item';
+const REACT_APP_IMAGE_EP='image';
+
+const REACT_APP_MANAGEMENT_API_URL='https://my.craftar.net/api/v0/';
+const REACT_APP_SEARCH_API_URL='https://search.craftar.net/v1/search';
+
+const REACT_APP_ES_PORT = 9243;
+
 const Config = {
     /** Craftar environment variables */
-    managementApiUrl: process.env.REACT_APP_MANAGEMENT_API_URL,
+	searchApiUrl: REACT_APP_SEARCH_API_URL,
+    managementApiUrl: REACT_APP_MANAGEMENT_API_URL,
     managementApiKey: process.env.REACT_APP_MANAGEMENT_API_KEY,
 
     collectionUuid: process.env.REACT_APP_COLLECTION_UUID,
     collectionToken: process.env.REACT_APP_COLLECTION_TOKEN,
 
-    searchApiUrl: process.env.REACT_APP_SEARCH_API_URL,
-
-    collectionRoute: process.env.REACT_APP_COLLECTION_EP,
-    itemRoute: process.env.REACT_APP_ITEM_EP,
-    imageRoute: process.env.REACT_APP_IMAGE_EP,
+    collectionRoute: REACT_APP_COLLECTION_EP,
+    itemRoute: REACT_APP_ITEM_EP,
+    imageRoute: REACT_APP_IMAGE_EP,
 
     /** ElasticSearch environment variables */
     esHost: process.env.REACT_APP_ES_HOST,
-    esPort: process.env.REACT_APP_ES_PORT,
+    esPort: REACT_APP_ES_PORT,
     esUsername: process.env.REACT_APP_ES_USERNAME,
     esPassword: process.env.REACT_APP_ES_PASSWORD
 }
