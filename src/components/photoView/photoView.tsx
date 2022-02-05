721e79b1524a4bf8c8ac58e0079c11a0a367f8f7
Christopher Jativa <cjativa@barnesfoundation.org>
2022-02-05 12:22:51 -0500

    file formatting

--- src/components/photoView/photoView.tsx
+++ src/components/photoView/photoView.tsx
@@ -1,16 +1,21 @@
-import * as React from 'react'
+import * as React from "react";
 
 export interface Props {
-    photoUri: string,
-    photoRef?: any
+    photoUri: string;
+    photoRef?: any;
 }
 
 export class PhotoView extends React.Component<Props, object> {
-
     public render() {
         return (
             <div className="photoviewer">
-                <img id="captured-photo" className="captured-photo" ref={this.props.photoRef} src={this.props.photoUri}></img>            </div>
-        )
+                <img
+                    id="captured-photo"
+                    className="captured-photo"
+                    ref={this.props.photoRef}
+                    src={this.props.photoUri}
+                />{" "}
+            </div>
+        );
     }
 }
