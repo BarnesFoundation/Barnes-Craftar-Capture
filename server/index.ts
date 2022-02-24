d882f685f39ce068c8e7263bb6aa9e2fce099b17
Christopher Jativa <cjativa@barnesfoundation.org>
2022-02-24 00:40:34 -0500

    use api router

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,26 @@
-import express from 'express'
-import * as path from 'path'
-import { Config } from './config'
+import express from "express";
+import * as path from "path";
+import { Config } from "./config";
+import { ApiRouter } from "./routes";
 
 /** Set up server constants */
-const app = express()
-const port = Config.port
-const buildDir = path.join(__dirname, '../build')
-
+const app = express();
+const buildDir = path.join(__dirname, "../build");
 
 /** Express serves the build directory */
-app.use(express.static(buildDir, { index: false, etag: false }))
+app.use(express.static(buildDir, { index: false, etag: false }));
 
-app.use('*', (request, response) => {
-    response.sendFile(path.join(buildDir, 'index.html'));
-})
+app.use("/api", ApiRouter);
+
+app.use("*", (request, response) => {
+    response.sendFile(path.join(buildDir, "index.html"));
+});
 
 // Only start the server when working locally
-if (Config.nodeEnv === 'LOCAL') {
-	app.listen(port, () => {
-		console.log('Server running on port:' , port)
-	});
+if (Config.nodeEnv === "LOCAL") {
+    app.listen(Config.port, () => {
+        console.log("Server running on port:", Config.port);
+    });
 }
 
 export default app;
