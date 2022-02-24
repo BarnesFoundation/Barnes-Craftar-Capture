bafb9e5a7f99a4ebd24e525d8b24a7e494ede2a9
Christopher Jativa <cjativa@barnesfoundation.org>
2022-02-24 00:41:00 -0500

    moved elastic search code to backend

--- src/services/invnoSearchService.ts
+++ src/services/invnoSearchService.ts
@@ -1,89 +1,42 @@
-import * as ElasticSearch from 'elasticsearch'
-import { ItemSearchService, SearchResponse } from './itemSearchService'
-import { Config } from '../utils/config'
-
-class ElasticSearchResponse {
-    success: boolean
-    id: string
-    invno: string
-}
+import axios from "axios";
+import { ItemSearchService, SearchResponse } from "./itemSearchService";
 
 class InvnoResponse {
-    idResponse?: SearchResponse | any
-    success: boolean
-    id?: string
+    idResponse?: SearchResponse | any;
+    success: boolean;
+    id?: string;
 }
 
 class InvnoSearchService {
-
-    esClient: ElasticSearch.Client
-    itemSearchService: ItemSearchService
+    itemSearchService: ItemSearchService;
 
     constructor() {
-
-        this.itemSearchService = new ItemSearchService()
-
-        const protocol = 'https://'
-        const address = Config.esHost + ':' + Config.esPort
-        const httpAuth = Config.esUsername + ':' + Config.esPassword
-
-        const host = protocol + httpAuth + '@' + address
-
-        this.esClient = new ElasticSearch.Client({
-            hosts: [host]
-        })
+        this.itemSearchService = new ItemSearchService();
     }
 
     async searchByInvno(invno: string): Promise<InvnoResponse> {
+        const response = await axios({
+            method: "GET",
+            url: "/api/search-by-invno",
+            params: {
+                invno: invno,
+            },
+        });
 
-        // Get the results this invno
-        let results = await this.getId(invno)
-
-        let invnoResponse = new InvnoResponse()
-
-        if (results.length > 0) {
-            let result = results[0]._source as ElasticSearchResponse
-            let id = result.id
-
-            invnoResponse.success = true
-            invnoResponse.idResponse = await this.itemSearchService.searchByItem(id)
-            invnoResponse.id = id
+        if (response.status === 200) {
+            return {
+                ...response.data,
+                idResponse: await this.itemSearchService.searchByItem(
+                    response.data.id
+                ),
+            };
         }
 
-        else {
-            invnoResponse.success = false
-            invnoResponse.idResponse = { }
-        }
-
-        return invnoResponse
-    }
-
-    private async getId(invno: string): Promise<any> {
-
-        // Execute the search
-        const response = await this.esClient.search({
-            index: 'collection',
-            body: {
-                size: 1,
-                query: {
-                    bool: {
-                        must: {
-                            match: {
-                                'invno': invno
-                            }
-                        }
-                    }
-                },
-                _source: [
-                    'id',
-                    'invno'
-                ]
-            }
-        })
-
-        // Return the actual object data
-        return response.hits.hits
+        return {
+            ...response.data,
+            idResponse: {},
+        };
     }
 }
 
-export { InvnoSearchService, InvnoResponse }
+export { InvnoSearchService, InvnoResponse };
