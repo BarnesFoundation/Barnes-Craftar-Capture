db1a92a32574c999e390880af8f95848d8a70dfd
Christopher Jativa <cjativa@barnesfoundation.org>
2023-06-21 18:50:00 -0400

    refactor SearchService a bit

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -1,50 +1,29 @@
+import axios from "axios";
+
 import { Config } from "../utils/config";
 import { SearchResponse } from "../interfaces/imageRecognitionResponses";
-import axios from "axios";
 
 class MatchResponse {
   success: boolean;
   message: string;
+
+  // These are only provide when the request is successful in finding a match
   id?: string;
   uuid?: string;
-
-  errorOccurred?: boolean;
-  errorMessage?: string;
 }
 
 class SearchService {
-  httpConfig;
-
-  constructor() {
-    this.httpConfig = {
-      headers: { "Content-Type": "multipart/form-data" },
-      method: "post",
-      url: Config.searchApiUrl,
-      data: null,
-    };
-  }
-
-  private prepareRequest(image: Blob) {
-    let fd = new FormData();
-
-    fd.append("token", Config.collectionToken);
-    fd.append("image", image);
-
-    this.httpConfig.data = fd;
-
-    return this.httpConfig;
-  }
-
   private parseImageMatchResponse(response: SearchResponse): MatchResponse {
-    let matchResponse = new MatchResponse();
+    const matchResponse = new MatchResponse();
 
     if (response.results.length > 0) {
-      let result = response.results[0];
+      // We only care about the first result
+      const result = response.results[0];
 
       matchResponse.success = true;
       matchResponse.message = "A matching reference image was found";
       matchResponse.id = result.item.name;
-      matchResponse.uuid = result.item.uuid;
+      matchResponse.uuid = result.item.name;
     } else {
       matchResponse.success = false;
       matchResponse.message = "No matching reference image was found";
@@ -52,24 +31,26 @@ class SearchService {
     return matchResponse;
   }
 
-  private parseErrorResponse(error): string {
-    let status = error.response.status;
-    let errorCode = error.response.data.error.code;
-    let errorMessage = error.response.data.error.message;
-
-    return "ErrorCode: " + errorCode + " ErrorMessage: " + errorMessage;
-  }
-
   async findImageMatch(image: Blob): Promise<MatchResponse> {
-    let request = this.prepareRequest(image);
+    const form = new FormData();
+    form.append("token", Config.collectionToken);
+    form.append("image", image);
 
     try {
-      let response = await axios(request);
-      let matchResponse = this.parseImageMatchResponse(response.data);
-      return matchResponse;
-    } catch (error) {
-      let message = this.parseErrorResponse(error);
-      throw message;
+      const response = await axios({
+        headers: { "Content-Type": "multipart/form-data" },
+        method: "post",
+        url: Config.searchApiUrl,
+        data: form,
+      });
+
+      return this.parseImageMatchResponse(response.data);
+    } catch (error: any) {
+      console.log(
+        "An error occurred while identifying a match for the image",
+        error
+      );
+      throw new Error("An error occurred finding a match for the image");
     }
   }
 }
