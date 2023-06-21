c299f3cafd440b72d59857fe96fbf601e93c6c8c
Christopher Jativa <cjativa@barnesfoundation.org>
2023-06-21 13:01:52 -0400

    export controllers from index and use in routes

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,17 @@
 import express from "express";
 
-import { SearchController } from "./controllers";
+import {
+  SearchController,
+  ManagementController,
+  uploadMiddleware,
+  MULTER_FIELD_NAME,
+} from "./controllers";
 
 export const ApiRouter = express.Router();
 
 ApiRouter.use("/search-by-invno", SearchController.searchByInvno);
+ApiRouter.post(
+  "/add-image",
+  uploadMiddleware.single(MULTER_FIELD_NAME),
+  ManagementController.addReferenceImage
+);
