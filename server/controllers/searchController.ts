b60277fe5919bf9d05fff002b51f822e7287a6d9
Christopher Jativa <cjativa@barnesfoundation.org>
2023-06-21 16:39:00 -0400

    add endpoint and controller function for searching by image

--- server/controllers/searchController.ts
+++ server/controllers/searchController.ts
@@ -2,11 +2,25 @@ import express from "express";
 import * as ElasticSearch from "elasticsearch";
 
 import { Config } from "../config";
+import { VuforiaClient as vuforiaClient } from "../vuforiaClient";
 
 interface ElasticSearchSource {
   success: boolean;
   id: string;
   invno: string;
+}
+
+interface VuforiaResponse {
+  query_id: string;
+  results: Array<{
+    target_id: string;
+    target_data: {
+      name: string;
+      application_metadata: string;
+      target_timestamp: number;
+    };
+  }>;
+  result_code: string;
 }
 
 const esClient = new ElasticSearch.Client({
@@ -54,6 +68,62 @@ class SearchController {
       id: null,
       success: false,
     });
+  }
+
+  public static async searchByImage(
+    request: express.Request,
+    response: express.Response
+  ) {
+    const queryImage = request.file;
+    const imageBinary = queryImage.buffer.toString("binary");
+
+    try {
+      const imageSearchResult = await new Promise<VuforiaResponse>(
+        (resolve, reject) => {
+          vuforiaClient.cloudRecoQuery(
+            imageBinary,
+            5,
+            function (error, result) {
+              if (error) {
+                return reject(error);
+              } else {
+                return resolve(result);
+              }
+            }
+          );
+        }
+      );
+
+      console.log(
+        "Successful Vuforia API image search result",
+        JSON.stringify(imageSearchResult)
+      );
+
+      // Transform the result into a format similar to how CraftAR was providing us
+      // so that we can continue to utilize the FE as-is without any changes for now
+      const transformedResult = {
+        search_time: Date.now(),
+        results: imageSearchResult.results
+          // Not sure why some items in this list come back with no `target_data` key
+          // If there is no `target_data` key then we don't get a `name` value
+          .filter((result) => result.target_id && result.target_data)
+          .map((result) => ({
+            item: {
+              name: result.target_data?.name,
+            },
+            image: {
+              thumb_120: "null",
+            },
+          })),
+      };
+
+      return response.status(200).json(transformedResult);
+    } catch (error) {
+      console.log(`An error occurred sending request to Vueforia`, error);
+      return response.status(500).json({
+        message: "An error occurrend sending performing image search request",
+      });
+    }
   }
 }
 
--- server/routes.ts
+++ server/routes.ts
@@ -11,6 +11,11 @@ export const ApiRouter = express.Router();
 
 ApiRouter.use("/search-by-invno", SearchController.searchByInvno);
 ApiRouter.post(
+  "/search-by-image",
+  uploadMiddleware.single(MULTER_FIELD_NAME),
+  SearchController.searchByImage
+);
+ApiRouter.post(
   "/add-image",
   uploadMiddleware.single(MULTER_FIELD_NAME),
   ManagementController.addReferenceImage
