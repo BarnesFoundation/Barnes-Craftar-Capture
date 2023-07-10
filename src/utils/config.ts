0677a04fcc510ec071246d8564d35de373e93852
Chris Jativa <cjativa@barnesfoundation.org>
2023-07-09 23:40:20 -0400

    remove unused variables

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,5 @@
 const Config = {
-  collectionUuid: process.env.REACT_APP_COLLECTION_UUID,
-  collectionToken: process.env.REACT_APP_COLLECTION_TOKEN,
+  collectionToken: '',
 };
 
 export { Config };
